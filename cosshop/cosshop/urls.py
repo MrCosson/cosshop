4cef42c094f5c2a07b0488bdf1428432525d2589
adrien.cosson <adrien.cosson@epita.fr>
2025-07-28 14:47:56 +0200

    d

--- cosshop/cosshop/urls.py
+++ cosshop/cosshop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
